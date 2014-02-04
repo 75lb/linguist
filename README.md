ef22b0ac3bf26662f1f2618a0cde69c189bdb13d
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-02-04 22:33:31 +0100

    more

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 #linguist
+
+##Command line
+###Install
+```sh
+$ npm install -g linguist
+$ translate --from en --to it "hello, how are you" "this is a nice cat"
+ciao, come stai
+questo è un bel gatto
+```
+
+###Usage
+####translate text at the command line
+```sh
+$ translate --from en --to it "hello, how are you" "this is a nice cat"
+ciao, come stai
+questo è un bel gatto
+```
+
+####translate the values in a json file
+with an input file like this:
+```json
+{
+    "content": {
+        "Terms": "Terms and Conditions",
+        "More": {
+            "Account": "Some Account",
+            "Products": {
+                "One": "Some Product"
+            }
+        }
+    }
+}
+```
+
+this command 
+```sh
+$ translate --input input.json --output output.json --from en --to it
+```
+
+will produce this output file: 
+```json
+{
+    "content": {
+        "Terms": "Termini e Condizioni",
+        "More": {
+            "Account": "Alcuni Account",
+            "Products": {
+                "One": "Alcune Prodotto"
+            }
+        }
+    }
+}
+```
+
+##Library
+```sh
+$ npm install linguist --save
+```
+
+```js
+var linguist = require("linguist");
+
+linguist.translate("hello, how are you", "en", "it", function(translation){
+    console.log(translation);
+});
+```
--- linguist.js
+++ linguist.js
@@ -8,13 +8,35 @@ var yandex = require("./lib/yandex"),
     l = console.log;
 
 var options = new Thing()
-    .define({ name: "from", type: "string", alias: "f" })
-    .define({ name: "to", type: "string", alias: "t" })
+    .define({ name: "from", type: "string", alias: "f", value: "en" })
+    .define({ name: "to", type: "string", alias: "t", value: "en" })
     .define({ name: "text", type: Array, defaultOption: true })
+    .define({ name: "input", type: "string", alias: "i" })
+    .define({ name: "output", type: "string", alias: "o" })
     .set(process.argv);
 
+function translateObject(obj){
+    Object.keys(obj).forEach(function(word){
+        var text = obj[word];
+        if(typeof text === "string"){
+            l("translating: " + text);
+            yandex.translate(text, options.from, options.to, function(translation){
+                obj[word] = translation.text[0];
+                fs.writeFileSync(output, JSON.stringify(input, null, "    "));
+            });
+        } else {
+            translateObject(obj[word]);
+        }
+    });
+}
+
 if (options.valid){
-    if (options.from || options.to){
+    if (options.input){
+        var input = require(path.join(process.cwd(), options.input)),
+            output = path.join(process.cwd(), options.output);
+        
+        translateObject(input);
+    } else if (options.from || options.to){
         yandex.translate(options.text, options.from, options.to, function(translation){
             console.log(translation.text.join("\n"));
         });
@@ -26,22 +48,3 @@ if (options.valid){
     l(options.validationMessages);
 }
 
-// var input = require(path.join(process.cwd(), process.argv[2])),
-//     output = path.join(process.cwd(), process.argv[3]);
-// 
-// function translateObject(obj){
-//     Object.keys(obj).forEach(function(word){
-//         var text = obj[word];
-//         if(typeof text === "string"){
-//             obj[word] = "translating...";
-//             yandex.translate(text, "en", "it", function(translation){
-//                 obj[word] = translation.text[0];
-//                 fs.writeFileSync(output, JSON.stringify(input, null, "    "));
-//             });
-//         } else {
-//             translateObject(obj[word]);
-//         }
-//     });
-// }
-// 
-// translateObject(input);
