99f57f6da7037812cba2a8d0e6a23c2dae3ae2df
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-02-04 22:04:27 +0100

    wip

--- linguist.js
+++ linguist.js
@@ -7,10 +7,24 @@ var yandex = require("./lib/yandex"),
     fs = require("fs"),
     l = console.log;
 
+var options = new Thing()
+    .define({ name: "from", type: "string", alias: "f" })
+    .define({ name: "to", type: "string", alias: "t" })
+    .define({ name: "text", type: Array, defaultOption: true })
+    .set(process.argv);
 
-yandex.translate(process.argv[2], process.argv[3], process.argv[4], function(translation){
-    console.log(translation.text[0]);
-});
+if (options.valid){
+    if (options.from || options.to){
+        yandex.translate(options.text, options.from, options.to, function(translation){
+            console.log(translation.text.join("\n"));
+        });
+    } else {
+        l("Invalid");
+    }
+} else {
+    l("Invalid");
+    l(options.validationMessages);
+}
 
 // var input = require(path.join(process.cwd(), process.argv[2])),
 //     output = path.join(process.cwd(), process.argv[3]);
